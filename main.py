145c289a1ffe69ac3074496b249d872a3eacef0d
vsnioana <visanioana980@gmail.com>
2025-03-20 21:46:25 +0200

    deduplicate entries with the same domain and title

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
 
-# Load the dataset
-df = pd.read_parquet('veridion_product_deduplication_challenge.snappy.parquet')
-print(df.head())
+def printCount(label, df):
+    print("Entries " + label + ":\t" + str(len(df)))
+
+# Load the dataset.
+df = pd.read_parquet('veridion_product_deduplication_challenge.snappy.parquet', engine='pyarrow')
+printCount("before processing", df)
+
+# Same product title on the same domain should be duplicate.
+df = df.drop_duplicates(['root_domain', 'product_title'])
+printCount("after deduplication", df)
